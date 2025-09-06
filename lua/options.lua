68d942445e46750518a374ffc35500afa3ae8292
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-09-06 10:49:59 -0400

    Fix tabs and spaces

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,8 @@ vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-
+vim.opt.expandtab = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+
