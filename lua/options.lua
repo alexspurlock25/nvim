166f061ff503db74994a7ae7fb2166e42fc3f612
Alexander Spurlock <alexanderspurlock@gmail.com>
2025-09-05 22:55:00 -0400

    First commit

---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.cmd("colorscheme default")
+vim.cmd("highlight Normal ctermbg=none guibg=none")
